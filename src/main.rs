4f614b0e472e6c097a789e3248b0e4040d781efb
antbern <40672068+antbern@users.noreply.github.com>
2024-10-13 18:05:52 +0200

    simple folder traversal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::VecDeque, path::Path};
+
+fn main() -> anyhow::Result<()> {
+    let folder = std::env::args().nth(1).expect("Please provide a folder");
+    traverse_folder(&Path::new(&folder))?;
+
+    Ok(())
+}
+
+fn traverse_folder(folder: &Path) -> anyhow::Result<()> {
+    let mut todo = VecDeque::new();
+    todo.push_back(folder.to_path_buf());
+
+    while let Some(path) = todo.pop_front() {
+        if path.is_dir() {
+            for entry in path.read_dir()? {
+                let entry = entry?;
+                let path = entry.path();
+                todo.push_back(path);
+            }
+        } else {
+            println!("{}", path.display());
+        }
+    }
+
+    Ok(())
 }
